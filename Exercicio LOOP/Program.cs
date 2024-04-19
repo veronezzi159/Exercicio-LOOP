67e3d2aca08a8cf2ad942e42f16b6495e67b5698
veronezzi159 <lucasveronezzi3@gmail.com>
2024-04-19 00:54:34 -0300

    Add project files.

---
+++ Exercicio LOOP/Program.cs
@@ -0,0 +1,39 @@
+﻿int inicial = 0, final = 0, contador = 0, max = 0, aux = 0;
+
+Console.WriteLine("Digite um numero inicial");
+inicial = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite um numero final");
+final = int.Parse(Console.ReadLine());
+
+max = final - inicial;
+aux = inicial;
+Console.WriteLine("Numero de partida: " + inicial);
+do
+{
+    aux++;
+    if ( aux < final) 
+    {        
+        if (aux % 2 == 0)
+        {
+            if (aux == 0)
+            {
+                Console.WriteLine($"{aux} é neutro");
+            }
+            else
+            { 
+                Console.WriteLine($"{aux} é par");
+            }
+
+        }
+        else
+        {
+            Console.WriteLine($"{aux} é ímpar");
+        }
+        contador++;
+    }
+} while (contador < (max -1));
+Console.WriteLine(max);
+Console.WriteLine(contador);
+
+Console.WriteLine("Numero de encerramento: " + final);
